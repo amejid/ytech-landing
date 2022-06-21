9f60fd26c7e4f6c7d781e5997af8ac1e7c994ce9
Abdelmejid Oumer Ali <abdelmejid89@gmail.com>
2022-06-21 09:14:36 +0300

    Render team member contents dynamically

--- js/main.js
+++ js/main.js
@@ -50,3 +50,24 @@ const teamInfo = [
     imageUrl: '../imgs/1.png',
   },
 ];
+
+document.addEventListener('DOMContentLoaded', () => {
+  teamInfo.forEach((member) => {
+    let builder = '';
+    builder = `
+      <div class="member-img-container">
+        <img class="member-img" src="imgs/1.png" alt="" />
+      </div>
+      <div class="member-content">
+        <h4 class="member-name">${member.name}</h4>
+        <p class="member-job"><em>${member.position}</em></p>
+        <div class="indicator"></div>
+        <p class="member-desc">${member.description}</p>
+      </div>`;
+
+    const memberEl = document.createElement('div');
+    memberEl.innerHTML = builder;
+    memberEl.classList.add('member');
+    teamEl.appendChild(memberEl);
+  });
+});

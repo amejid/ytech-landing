77e56095529c8d1913f6be01086c1f2f9335a309
Abdelmejid Oumer Ali <abdelmejid89@gmail.com>
2022-06-21 09:13:44 +0300

    Create array of objects for team info

---
+++ js/main.js
@@ -0,0 +1,52 @@
+const teamEl = document.querySelector('.team');
+
+const teamInfo = [
+  {
+    name: 'Yochai Benkler',
+    position: 'Fullstack',
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam cupiditate ex esse
+    sapiente. Distinctio quae adipisci ipsum sed nihil iste tenetur, minus laboriosam,
+    quos animi eligendi eos, dignissimos perferendis nesciunt?`,
+    imageUrl: '../imgs/1.png',
+  },
+  {
+    name: 'Yochai Benkler',
+    position: 'Fullstack Senior',
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam cupiditate ex esse
+    sapiente. Distinctio quae adipisci ipsum sed nihil iste tenetur, minus laboriosam,
+    quos animi eligendi eos, dignissimos perferendis nesciunt?`,
+    imageUrl: '../imgs/1.png',
+  },
+  {
+    name: 'Yochai Benkler',
+    position: 'Fullstack',
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam cupiditate ex esse
+    sapiente. Distinctio quae adipisci ipsum sed nihil iste tenetur, minus laboriosam,
+    quos animi eligendi eos, dignissimos perferendis nesciunt?`,
+    imageUrl: '../imgs/1.png',
+  },
+  {
+    name: 'Yochai Benkler',
+    position: 'Fullstack',
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam cupiditate ex esse
+    sapiente. Distinctio quae adipisci ipsum sed nihil iste tenetur, minus laboriosam,
+    quos animi eligendi eos, dignissimos perferendis nesciunt?`,
+    imageUrl: '../imgs/1.png',
+  },
+  {
+    name: 'Yochai Benkler',
+    position: 'Fullstack',
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam cupiditate ex esse
+    sapiente. Distinctio quae adipisci ipsum sed nihil iste tenetur, minus laboriosam,
+    quos animi eligendi eos, dignissimos perferendis nesciunt?`,
+    imageUrl: '../imgs/1.png',
+  },
+  {
+    name: 'Yochai Benkler',
+    position: 'Fullstack',
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam cupiditate ex esse
+    sapiente. Distinctio quae adipisci ipsum sed nihil iste tenetur, minus laboriosam,
+    quos animi eligendi eos, dignissimos perferendis nesciunt?`,
+    imageUrl: '../imgs/1.png',
+  },
+];

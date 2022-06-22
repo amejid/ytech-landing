c5635a4a2346e39fe28ab66737282ca2e0cb5dea
Abdelmejid Oumer Ali <abdelmejid89@gmail.com>
2022-06-22 12:40:36 +0300

    Move functionality of rendering clients to popup js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 const teamEl = document.querySelector('.team');
 const servicesEl = document.querySelector('.services');
-const clientsEl = document.querySelector('.clients');
 
 const teamInfo = [
   {
@@ -85,29 +84,6 @@ const servicesInfo = [
   },
 ];
 
-const clientsInfo = [
-  {
-    link: 'https://www.netflix.com',
-    imgUrl: ['imgs/netflix.svg', 'Netflix logo'],
-  },
-  {
-    link: 'https://www.google.com',
-    imgUrl: ['imgs/google.svg', 'Google logo'],
-  },
-  {
-    link: 'https://www.cocacola.com',
-    imgUrl: ['imgs/cocacola.svg', 'CocaCola logo'],
-  },
-  {
-    link: 'https://www.microsoft.com',
-    imgUrl: ['imgs/microsoft.svg', 'Microsoft logo'],
-  },
-  {
-    link: 'https://www.paypal.com',
-    imgUrl: ['imgs/paypal.svg', 'PayPal logo'],
-  },
-];
-
 document.addEventListener('DOMContentLoaded', () => {
   teamInfo.forEach((member) => {
     let builder = '';
@@ -139,18 +115,5 @@ document.addEventListener('DOMContentLoaded', () => {
     serviceEl.innerHTML = builder;
     serviceEl.classList.add('service');
     servicesEl.appendChild(serviceEl);
-  });
-
-  clientsInfo.forEach((client) => {
-    let builder = '';
-    builder = `
-      <a href="${client.link}">
-        <img class="client-logo" src="${client.imgUrl[0]}" alt="${client.imgUrl[1]}"/>
-      </a>`;
-
-    const clientEl = document.createElement('li');
-    clientEl.innerHTML = builder;
-    clientEl.classList.add('client');
-    clientsEl.appendChild(clientEl);
   });
 });
--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,6 @@
 const yearEl = document.querySelector('.year');
 const headerEl = document.querySelector('.header');
+const clientsEl = document.querySelector('.clients');
 const btnOpenEl = document.querySelector('.icon-open');
 const btnCloseEl = document.querySelector('.icon-close');
 
@@ -13,4 +14,42 @@ btnOpenEl.addEventListener('click', () => {
 btnCloseEl.addEventListener('click', () => {
   headerEl.classList.remove('nav-open');
   document.body.style.overflow = 'scroll';
+});
+
+const clientsInfo = [
+  {
+    link: 'https://www.netflix.com',
+    imgUrl: ['../imgs/netflix.svg', 'Netflix logo'],
+  },
+  {
+    link: 'https://www.google.com',
+    imgUrl: ['../imgs/google.svg', 'Google logo'],
+  },
+  {
+    link: 'https://www.cocacola.com',
+    imgUrl: ['../imgs/cocacola.svg', 'CocaCola logo'],
+  },
+  {
+    link: 'https://www.microsoft.com',
+    imgUrl: ['../imgs/microsoft.svg', 'Microsoft logo'],
+  },
+  {
+    link: 'https://www.paypal.com',
+    imgUrl: ['../imgs/paypal.svg', 'PayPal logo'],
+  },
+];
+
+document.addEventListener('DOMContentLoaded', () => {
+  clientsInfo.forEach((client) => {
+    let builder = '';
+    builder = `
+          <a href="${client.link}">
+            <img class="client-logo" src="${client.imgUrl[0]}" alt="${client.imgUrl[1]}"/>
+          </a>`;
+
+    const clientEl = document.createElement('li');
+    clientEl.innerHTML = builder;
+    clientEl.classList.add('client');
+    clientsEl.appendChild(clientEl);
+  });
 });

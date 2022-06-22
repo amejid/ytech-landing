fc3ef2c6f072f65281d48451569f30b42fa48c4a
Abdelmejid Oumer Ali <abdelmejid89@gmail.com>
2022-06-22 09:11:28 +0300

    Implement mobile menu functionality

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ const teamEl = document.querySelector('.team');
 const servicesEl = document.querySelector('.services');
 const clientsEl = document.querySelector('.clients');
 const yearEl = document.querySelector('.year');
+const headerEl = document.querySelector('.header');
+const btnOpenEl = document.querySelector('.icon-open');
+const btnCloseEl = document.querySelector('.icon-close');
 
 yearEl.textContent = new Date().getFullYear();
 
@@ -156,4 +159,14 @@ document.addEventListener('DOMContentLoaded', () => {
     clientEl.classList.add('client');
     clientsEl.appendChild(clientEl);
   });
+});
+
+btnOpenEl.addEventListener('click', () => {
+  headerEl.classList.add('nav-open');
+  document.body.style.overflow = 'hidden';
+});
+
+btnCloseEl.addEventListener('click', () => {
+  headerEl.classList.remove('nav-open');
+  document.body.style.overflow = 'scroll';
 });
